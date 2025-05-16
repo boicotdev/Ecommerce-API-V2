4b3291ee045d521508298468b0b4733c46609104
boicotdev <carlos.guzman93@live.com>
2025-05-15 21:28:35 -0500

    new requirements

--- api/api/settings.py
+++ api/api/settings.py
@@ -140,16 +140,6 @@ if DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-    # Static files (CSS, JavaScript, Images)
-    # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-    # This setting informs Django of the URI path from which your static files will be served to users
-    # Here, they will be accessible at your-domain.onrender.com/static/... or yourcustomdomain.com/static/...
-    # This production code might break development mode, so we check whether we're in DEBUG mode
-    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
